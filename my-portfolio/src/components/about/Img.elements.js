b17664a46cd2325648d58b8855211e0980b7ccaf
xavimagine <sr.xavier5@gmail.com>
2022-05-04 14:34:50 +0200

    Modification

--- my-portfolio/src/components/about/Img.elements.js
+++ my-portfolio/src/components/about/Img.elements.js
@@ -3,28 +3,38 @@ import styled from "styled-components";
 export const Containt = styled.div`
     width: 100%;
     height: 70px;
- 
-`;
-export const Perfil = styled.div`
-    img{
-        height: 10%;
-        width: 50%;
+
+    #pc{
+        width: 700px;
+        height: auto;
+        margin: 10px auto -1px;
+        display: block;
+       
     }
  
 `;
-export const Front = styled.div`
+export const Perfil = styled.div`
+    background-color: #23394d;
+    color: #FFFFFF;
+    text-align: center;
+    height: 250px;
+    padding-bottom: 14rem;
+    padding-top: 7%;
     
- 
+    h1{
+        font-family: 'Exo 2', sans-serif;
+    }
+    #Atext{
+        margin: 20px auto;
+        width: 500px;
+        text-align: justify;
+        
+        font-family: 'Smooch Sans', sans-serif;
+        font-size: 18px;
+    }
 `;
-export const Back = styled.div`
-  
- 
-`;
-export const Programs = styled.div`
-   
- 
-`;
-export const ProgramasD = styled.div`
-    
- 
+export const Material = styled.div`
+    /*border-radius: 12px;
+    margin-top: -14rem;
+    padding-bottom: 1rem;*/
 `;
--- my-portfolio/src/routesImg/imgRoutes.js
+++ my-portfolio/src/routesImg/imgRoutes.js
@@ -1,35 +1,8 @@
-import android from '../galery/android.png'
-import code from '../galery/code.png'
-import html  from '../galery/csshtmls.png'
-import  eclipse from '../galery/eclipse.png'
-import illustrator from '../galery/illustrator.png'
-import indesign from '../galery/ind.png'
-import  java from '../galery/java.png'
-import js  from '../galery/js.png'
-import mysql from '../galery/mysql.png'
-import phpmyadmin from '../galery/phpmyadmin.png'
-import postgresql from '../galery/postgresql.png'
-import photoshop from '../galery/ps.png'
-import reat from '../galery/react.png'
-import wordpress from '../galery/wordpress.png'
-
+import pcs from'../galery/pcs.png';
 import me from'../galery/me.png'
 
 const urls = {
-    'android': android,
-    'code': code,
-    'html':html,
-    'eclipse':eclipse,
-    'illustrator':illustrator,
-    'indesign':indesign,
-    'java':java,
-    'js':js,
-    'mysql':mysql,
-    'phpmyadmin':phpmyadmin,
-    'postgresql':postgresql,
-    'photoshop':photoshop,
-    'reat':reat,
-    'wordpress':wordpress,
+    'pcs': pcs,
     'me':me
 };
 
